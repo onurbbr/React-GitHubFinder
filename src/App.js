52e1f29421a7bfaad7a977e7ab328cb72d9b23c2
Onur BÜBER <onurbuber@engineer.com>
2022-07-22 15:37:14 +0300

    react: added some components added UserItem some changes in Navbar.js
    Signed-off-by: Onur BÜBER <onurbuber@engineer.com>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import './App.css';
 import { Component, Fragment } from 'react';
 import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <Navbar title='GitHub Finder' icon='fab fa-github' />
+        <Navbar />
+        <UserItem />
       </Fragment>
     );
   }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Navbar extends Component {
+  static defaultProps = {
+    title: 'GitHub Finder',
+    icon: 'fab fa-github',
+  };
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+  };
+
   render() {
     return (
       <nav className='navbar bg-primary'>
