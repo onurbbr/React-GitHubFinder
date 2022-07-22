4c4177c33919dd7eb7766d88200a93adc4b84f9e
Onur BÜBER <onurbuber@engineer.com>
2022-07-22 17:01:14 +0300

    Added spinner component
    Signed-off-by: Onur BÜBER <onurbuber@engineer.com>

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import spinner from './spinner.gif';
+
+export const Spinner = () => (
+  <Fragment>
+    <img
+      src={spinner}
+      alt='Loading...'
+      style={{ width: '200px', margin: 'auto', display: 'block' }}
+    />
+  </Fragment>
+);
