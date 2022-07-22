16aaa8e63e53b4ae96c32a0a14c1e19fa53793ad
Onur BÜBER <onurbuber@engineer.com>
2022-07-22 16:27:46 +0300

    Added axios
    Signed-off-by: Onur BÜBER <onurbuber@engineer.com>

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Users from './components/users/Users';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    console.log(123);
+  }
   render() {
     return (
       <div className='App'>
