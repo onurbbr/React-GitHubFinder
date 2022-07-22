90103eb61498e805d17f3bb7416eedccb2f708a4
Onur BÜBER <onurbuber@engineer.com>
2022-07-22 14:32:31 +0300

    react: Use showname on project.
    Signed-off-by: Onur BÜBER <onurbuber@engineer.com>

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Component, Fragment } from 'react';
 
 class App extends Component {
   render() {
+    const name = 'John Doe';
+    const loading = false;
+    const showName = true;
     return (
       <Fragment>
-        <h1>Hello from React!</h1>
-        <h2>Goodbye !</h2>
+        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name} </h1>}
       </Fragment>
     );
   }
